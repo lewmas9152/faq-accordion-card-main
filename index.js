eda119db0292c917e7b84fee480dc37a19755824
lewmas9152 <smnjuguna22@gmail.com>
2023-06-29 18:59:59 +0300

    made some changes in the css

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let questionEL = document.getElementsByClassName(".question");   
-let answerEl = document.getElementsByClassName(".answer");  
+let questionEL = document.getElementsByClassName("question");   
+let answerEl = document.getElementsByClassName("answer");  
 var i;
 for (i = 0; i < questionEL.length; i++) {
   questionEL[i].addEventListener("click", function() {
