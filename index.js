49262953958a815da0f927b3de31f8d1126e315c
lewmas9152 <smnjuguna22@gmail.com>
2023-06-29 20:00:37 +0300

    made changes to js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-let questionEL = document.getElementsByClassName("question");   
-let answerEl = document.getElementsByClassName("answer");  
+let questionEL = document.getElementsByClassName("question");
+let answerEl = document.getElementsByClassName("answer");
 var i;
 for (i = 0; i < questionEL.length; i++) {
-  questionEL[i].addEventListener("click", function() {
+  questionEL[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var answer = this.nextElementSibling;
     if (answer.style.display === "block") {
       answer.style.display = "none";
+      var question = this;
+      question.style.fontWeight = "400";
     } else {
       answer.style.display = "block";
+   var question = this;
+   question.style.fontWeight = "700";
     }
   });
 }
