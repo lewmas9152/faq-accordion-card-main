a11e082bb35803e9cea100f49c95e7e5fea5d04e
lewmas9152 <smnjuguna22@gmail.com>
2023-07-05 11:42:03 +0300

    changed styling of image

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let questionEL = document.getElementsByClassName("question");
 let answerEl = document.getElementsByClassName("answer");
+let arrowEl = document.getElementsByClassName("arrow");
 var i;
 for (i = 0; i < questionEL.length; i++) {
   questionEL[i].addEventListener("click", function () {
@@ -9,10 +10,14 @@ for (i = 0; i < questionEL.length; i++) {
       answer.style.display = "none";
       var question = this;
       question.style.fontWeight = "400";
+      var arrow = this.lastElementChild;
+      arrow.style.transform = "rotate(0deg)";
     } else {
       answer.style.display = "block";
    var question = this;
    question.style.fontWeight = "700";
+   var arrow = this.lastElementChild;
+    arrow.style.transform = "rotate(180deg)";
     }
   });
 }
